35588089422f10c1855d08c8b85043e2ceff074f
Just-andreew <andrewsolitei@gmail.com>
2023-03-22 14:00:14 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
+	/**
+	 * main - this function prints _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
 
-/**
- * main - print putchar
- *
- * Return: 0
- */
 
-int main(void)
-{
+	int main(void)
+	{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,4 +18,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
+	}
+
