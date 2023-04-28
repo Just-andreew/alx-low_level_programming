fef317db20720f49b7010be60ceef44b7208251e
Andrew <andrewsolitei@gmail.com>
2023-04-28 18:59:32 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,22 @@
-global   main
-extern    printf
-main:
-mov   edi, format
-xor   eax, eax
-call  printf
-mov   eax, 0
-ret
-format: db `Hello, Holberton\n`,0
+section .data
+    msg db "Hello, Holberton!", 0
+    fmt db "%s\n", 0
 
+section .text
+    global main
+
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [msg]
+    lea rsi, [fmt]
+    xor eax, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    xor eax, eax
+    ret
