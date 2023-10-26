1eaf52829ee0c808340f851beab161e8ddf1ed90
Just-andreew <andrewsolitei@gmail.com>
2023-10-25 20:07:49 -0700

    100

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,17 +9,21 @@
     Returns:
         int: The perimeter of the island.
     """
-    perimeter = 0
+        perimeter = 0
+
     for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            actual_pos = grid[row][col]
-            if actual_pos == 1:
-                if row - 1 == -1 or grid[row - 1][col] == 0:
-                    perimeter += 1
-                if row + 1 == len(grid) or grid[row + 1][col] == 0:
-                    perimeter += 1
-                if col - 1 == -1 or grid[row][col - 1] == 0:
-                    perimeter += 1
-                if col + 1 == len(grid[row]) or grid[row][col + 1] == 0:
-                    perimeter += 1
+        for col in range(len(grid[row]):
+            if grid[row][col] == 1:
+                perimeter += 4  # Start with 4 sides.
+
+                # Check the neighbors (left, right, up, and down) and reduce the perimeter for each adjacent land.
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+                if row < len(grid) - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+                if col < len(grid[row]) - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1
+
     return perimeter
